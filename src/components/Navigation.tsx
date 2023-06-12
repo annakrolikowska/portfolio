e55dc92823c1b19a7414f73aaf042094d49496a5
Anna <krolikowska.ann@gmail.com>
2023-06-12 21:39:21 +0200

    updated navigation and created mobile version

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../assets/styles/components-styles/Navigation.css';
 import Highlight from './Highlight';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faXmark  } from '@fortawesome/free-solid-svg-icons'
 
 interface Link {
   title: string;
@@ -18,25 +18,47 @@ const Navigation: React.FC = () => {
   ];
 
   const [isOpen, setIsOpen] = useState(false);
+  const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 767);
+    };
+
+    handleResize(); 
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const handleClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, target: string) => {
     event.preventDefault();
     const element = document.getElementById(target);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
-      setIsOpen(false); 
+      setIsOpen(false);
     }
   };
 
   const handleHamburgerClick = () => {
     setIsOpen(!isOpen);
+    setIsMobileMenuVisible(!isMobileMenuVisible);
   };
 
   return (
-    <nav className={`navigation ${isOpen ? 'navigation--open' : ''}`}>
-      <div className="navigation__hamburger" onClick={handleHamburgerClick}>
-      <FontAwesomeIcon icon={faBars} />
-      </div>
+    <nav className={`navigation ${isMobile ? 'navigation--mobile' : ''} ${isOpen ? 'navigation--open' : ''}`}>
+      {isMobile ? (
+        <div className={`navigation__hamburger ${isOpen ? 'navigation__hamburger--open' : ''}`} onClick={handleHamburgerClick}>
+          {isMobileMenuVisible ? (
+            <FontAwesomeIcon icon={faXmark} />
+          ) : (
+            <FontAwesomeIcon icon={faBars} />
+          )}
+        </div>
+      ) : null}
       <ul className={`navigation__list ${isOpen ? 'navigation__list--open' : ''}`}>
         {links.map((link, index) => (
           <li key={index} className="navigation__item">
