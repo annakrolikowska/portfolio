5208e4d342a68919045c8fd86046e971cccf2416
Anna <krolikowska.ann@gmail.com>
2023-06-12 21:42:39 +0200

    updated Header section, added media queries

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -6,17 +6,20 @@ import Navigation from '../components/Navigation';
 
 const Header: React.FC = () => {
   return (
-    <section className="header" id="home">
+    <header className="header container" id="home">
         <Navigation/>
         <div className="header__text-container">
-        <h1>Hello, I’m Anna, and I’m Junior <span>Frontend&nbsp;Developer</span></h1>
-        <p>I am actively seeking new opportunities in the frontend development field.</p>
-        <Button>Checkout my projects</Button>
-        </div>
-        <div className="header__img-container">
+            <h1>Hello,<br/> I’m Anna, and I’m Junior <span>Frontend&nbsp;Developer</span></h1>
+            <div className="header__img-container header__img-container--mobile">
             <img className="header__img" src={avatar} alt="avatar" />
         </div>
-    </section>
+            <p>I am actively seeking new opportunities in the frontend development field.</p>
+            <Button href="#projects">Checkout my projects</Button>
+        </div>
+        <div className="header__img-container header__img-container--desktop">
+            <img className="header__img" src={avatar} alt="avatar" />
+        </div>
+    </header>
   );
 };
 
