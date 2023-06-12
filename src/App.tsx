6293b8974aae6a91f1d454040611287ffecac7dd
Anna <krolikowska.ann@gmail.com>
2023-06-12 21:45:39 +0200

    added all sections to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import './App.css';
+import Header from './sections/Header';
+import About from './sections/About';
+import Stack from './sections/Stack';
+import Projects from './sections/Projects';
+import Footer from './sections/Footer';
+import Contact from './sections/Contact';
 
 function App() {
   return (
     <div className="App">
-
+       <Header/>
+       <About/>
+       <Stack/>
+       <Projects/>
+       <Contact/>
+       <Footer/>
     </div>
   );
 }
