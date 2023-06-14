929448d92946a432df418cb7a20973fdace24769
Anna <krolikowska.ann@gmail.com>
2023-06-14 23:34:26 +0200

    added background to Header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -7,17 +7,18 @@ import { ReactComponent as BackgroundSVG } from '../assets/images/img-background
 
 const Header: React.FC = () => {
   return (
-    <header className="header container" id="home">
+    <header className="header container fade-in" id="home">
         <Navigation/>
-        <div className="header__text-container">
+        <div className="header__text-container fade-in">
             <h1>Hello,<br/> I’m Anna, and I’m Junior <span>Frontend&nbsp;Developer</span></h1>
             <div className="header__img-container header__img-container--mobile">
+            <BackgroundSVG className="header__img-background"  />
             <img className="header__img" src={avatar} alt="avatar" />
         </div>
             <p>I am actively seeking new opportunities in the frontend development field.</p>
             <Button href="#projects">Checkout my projects</Button>
         </div>
-        <div className="header__img-container header__img-container--desktop">
+        <div className="header__img-container header__img-container--desktop ">
             <BackgroundSVG className="header__img-background"  />
             <img className="header__img" src={avatar} alt="avatar" />
         </div>
